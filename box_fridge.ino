eea9bbffcf87e7891e2c16824cedacea988fa60b
Francis Duvivier <francisduvivier@users.noreply.github.com>
2022-06-28 22:29:56 +0200

    Fix latch working

--- box_fridge.ino
+++ box_fridge.ino
@@ -11,7 +11,10 @@
 Adafruit_SSD1306 ssd1306Scherm(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 //TEMP DHT22
-int wantedTemp = 10;
+float wantedTemp = 10;
+float wantedTempRange = 4;
+float MAXTEMP = 30;
+float MINTEMP = -5;
 #define DHTPIN 11
 #define DHTTYPE DHT22
 
@@ -19,6 +22,7 @@ int wantedTemp = 10;
 #define POTPIN A0
 //This allows to disable the potentiometer by NOT connecting d3 to ground.
 #define POTSWITCH 3
+#define RANGESWITCH 4
 
 //RELAY
 #define RELPIN 2
@@ -32,14 +36,16 @@ void setup() {
   pinMode(RELPIN, OUTPUT);
   pinMode(POTPIN, INPUT);
   pinMode(POTSWITCH, INPUT_PULLUP);
-
+  pinMode(RANGESWITCH, INPUT_PULLUP);
   dht.begin();
 }
+bool state = false;
+
 void loop() {
   int nbSame = 0;
   int nbSameNeeded = 3;
   float temp = dht.readTemperature(false);
-  if (!digitalRead(POTSWITCH)) {
+  if (!digitalRead(POTSWITCH) || !digitalRead(RANGESWITCH)) {
     int potVal = analogRead(POTPIN);
     int secondPotRead = -1;
     //Filter out bad potentiometer readings
@@ -47,15 +53,19 @@ void loop() {
       potVal = secondPotRead;
       secondPotRead = analogRead(POTPIN);
     }
-    wantedTemp = potVal / 30 - 5;
+    if (!digitalRead(RANGESWITCH)) {
+      wantedTempRange = potVal /(1024/10.0);
+    } else {
+      wantedTemp = potVal / (1024/(MAXTEMP-MINTEMP)) + MINTEMP;
+    }
+
   }
-  int offTemp = wantedTemp - 1;
-  int onTemp = wantedTemp + 1;
-  bool state = false;
-  if (temp < offTemp) {
-    state = false;
-  } else if (temp > onTemp) {
+  float lowTemp = wantedTemp - (wantedTempRange / 2);
+  float highTemp = wantedTemp + (wantedTempRange / 2);
+  if (temp < lowTemp) {
     state = true;
+  } else if (temp > highTemp) {
+    state = false;
   }
   digitalWrite(RELPIN, state);
 
@@ -64,19 +74,17 @@ void loop() {
   ssd1306Scherm.clearDisplay();
   ssd1306Scherm.setTextColor(WHITE);
 
-  // Show temp setting from POT
+// Show wanted pot
   ssd1306Scherm.setTextSize(1);
   ssd1306Scherm.setCursor(2, 0);
   ssd1306Scherm.print("wanted:");
+  String wantedTempString = String(wantedTemp, 1);
 
-  ssd1306Scherm.print(wantedTemp, DEC);
-  ssd1306Scherm.print("C->");
-
-
-  // Show wanted pot
-  ssd1306Scherm.print(offTemp, DEC);
-  ssd1306Scherm.print("::");
-  ssd1306Scherm.print(onTemp, DEC);
+  ssd1306Scherm.print(wantedTempString);
+  ssd1306Scherm.print("+-");
+  
+  String wantedTempMarginString = String(wantedTempRange/2, 1);
+  ssd1306Scherm.print(wantedTempMarginString);
 
   // Show TEMP
   ssd1306Scherm.setTextSize(2);
@@ -84,7 +92,7 @@ void loop() {
   String tempString = String(temp, 1);
   ssd1306Scherm.print(tempString);
   ssd1306Scherm.print("C::");
-  ssd1306Scherm.print(state ? "ON" : "OFF");
+  ssd1306Scherm.print(state ? "OFF" : "ON");
 
   ssd1306Scherm.setTextSize(1);
   ssd1306Scherm.setCursor(27, 23);
